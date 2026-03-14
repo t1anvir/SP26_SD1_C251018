2faf29ad16e267d1339769c8aff7f42db94d2c47
tanvir <tanvshuvo@gmail.com>
2026-03-15 01:48:33 +0600

    main

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int x = s.nextInt();
+        String str = s.nextLine();
+
+        System.out.println(x);
+        System.out.println(str);
+
+        s.close();
+    }
+}
